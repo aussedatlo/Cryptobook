752f2271bba3a65501014add518d1185105407b6
Louis Aussedat <aussedat.louis@gmail.com>
2021-09-01 23:25:37 +0200

    AddressList: remove Menu item since it's already in AddressScreen

--- app/component/AddressList.tsx
+++ app/component/AddressList.tsx
@@ -1,14 +1,8 @@
 import React, { useMemo } from "react";
 import { useNavigation } from "@react-navigation/native";
-import {
-  Image,
-  StyleSheet,
-  ToastAndroid,
-  TouchableNativeFeedback,
-} from "react-native";
+import { Image, StyleSheet, TouchableNativeFeedback } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { Card, Menu, useTheme } from "react-native-paper";
-import { useTranslation } from "react-i18next";
 import { Ionicons } from "@expo/vector-icons";
 import { Theme } from "react-native-paper/lib/typescript/types";
 import { observer } from "mobx-react-lite";
@@ -19,59 +13,37 @@ const AddressList = () => {
   const { navigate } = useNavigation();
   const theme = useTheme();
   const styles = useMemo(() => createStyles(theme), [theme]);
-  const { t } = useTranslation("common");
   const { addresses } = useStore();
 
   const RenderMenuItem = ({ item }: any) => {
-    const [visible, setVisible] = React.useState(false);
-    const openMenu = () => setVisible(true);
-    const closeMenu = () => setVisible(false);
-
-    const handleDelete = async () => {
-      addresses.removeAddress(item);
-      ToastAndroid.show(t("removed"), ToastAndroid.SHORT);
-      closeMenu();
-    };
-
     return (
-      <Menu
-        key={item.id}
-        visible={visible}
-        onDismiss={closeMenu}
-        style={styles.menu}
-        anchor={
-          <TouchableNativeFeedback
-            onPress={() => {
-              navigate("address", { address: item });
-            }}
-            onLongPress={openMenu}
-          >
-            <Card style={styles.card}>
-              <Card.Title
-                title={item.label}
-                subtitle={item.address}
-                left={(props) => (
-                  <Image
-                    source={{ uri: item.image }}
-                    width={30}
-                    height={30}
-                    style={styles.logo}
-                  />
-                )}
-                right={(props) => (
-                  <Ionicons
-                    style={styles.chevron}
-                    name={"chevron-forward"}
-                    size={16}
-                  />
-                )}
-              />
-            </Card>
-          </TouchableNativeFeedback>
-        }
+      <TouchableNativeFeedback
+        onPress={() => {
+          navigate("address", { address: item });
+        }}
       >
-        <Menu.Item onPress={handleDelete} title={t("delete")} />
-      </Menu>
+        <Card style={styles.card}>
+          <Card.Title
+            title={item.label}
+            subtitle={item.address}
+            left={(props) => (
+              <Image
+                source={{ uri: item.image }}
+                width={30}
+                height={30}
+                style={styles.logo}
+              />
+            )}
+            right={(props) => (
+              <Ionicons
+                style={styles.chevron}
+                name={"chevron-forward"}
+                size={16}
+              />
+            )}
+          />
+        </Card>
+      </TouchableNativeFeedback>
     );
   };
 
@@ -86,18 +58,11 @@ const AddressList = () => {
 
 const createStyles = (theme: Theme) => {
   return StyleSheet.create({
-    root: {
-      flex: 1,
-      backgroundColor: theme.colors.background,
-      paddingLeft: 20,
-      paddingRight: 20,
-    },
     chevron: {
       textAlignVertical: "center",
       marginRight: 10,
       color: theme.dark ? theme.colors.primary : theme.colors.accent,
     },
-    menu: { left: "auto", right: 10, marginTop: 60 },
     logo: {
       width: 30,
       height: 30,
